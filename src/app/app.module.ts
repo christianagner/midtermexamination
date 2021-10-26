14f7ef1e06192e4983b7be1eb4dc87ef9653509f
Christian Agner <christianagnerzxc@gmail.com>
2021-10-26 20:50:03 +0800

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    GalleryComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
